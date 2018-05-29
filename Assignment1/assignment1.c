2abf69d39c4b9e5a16dc05c8e5e7dd6e5e0a3db8
Tarunjoseph <tarun.joseph19@gmail.com>
2018-05-29 22:36:56 +0530

    Added problem statement

--- Assignment1/assignment1.c
+++ Assignment1/assignment1.c
@@ -1,3 +1,13 @@
+/*
+Pig Esperanto
+
+For each input word:
+
+    Move the first letter to the end.
+    Add the letters "an" to the end of any word of three letters or less, and "o" to the end of any longer word.
+    Make the new first letter of the word match the case of the old first letter of the word. Make the letter that was moved lowercase if it is not still the first letter. Do not change the capitalization of any other letters.
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
