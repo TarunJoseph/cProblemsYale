bacf907ab7feb9b607039383de389fedba507d63
Tarunjoseph <tarun.joseph19@gmail.com>
2018-07-10 13:08:44 +0530

    doubly linked list

--- basics/doublyLinkedList.c
+++ basics/doublyLinkedList.c
@@ -47,10 +47,31 @@ void Print()
     }
     printf("\n");
 }
+void reversePrint(void)
+{
+    Node* temp = head ;
+    
+    if(temp == NULL)
+        return;
+        
+    //traverse the list in forwar direction
+    while(temp->next != NULL){
+        temp = temp->next;
+    }    
+    printf("Backward : ");
+    while(temp != NULL){
+        printf("%d ",temp->data);
+        temp = temp->prev;
+    }
+    printf("\n");
+    
+}
 int main (int argc , char** argv)
 {
+    head = NULL;
     insertAtHead(5);
     insertAtHead(6);
     insertAtHead(9);
     Print(); 
+    reversePrint();
 }
